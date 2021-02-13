07fd41142eab3eb21ec17766b8bffaf961a71322
prashikjaware <prashikjaware123@gmail.com>
2021-02-13 12:58:22 +0530

    Added: Finding the singlet Combination Percentage of Head and Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,30 @@
 #!/bin/bash -x
 
-flip=$((RANDOM%2))
-HEAD=1
+read -p "Enter how many times to flip the coin: " num
 
-if [ $HEAD -eq $flip ]
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+declare -A dict
+HEAD=0
+TAIL=0
+count=0
+while [ $count -ne $num ]
+do
+	flip=$((RANDOM%2))
+
+	if [ 1 -eq $flip ]
+	then
+		((HEAD++))
+		dict[$count]="H"
+	else
+		((TAIL++))
+		dict[$count]="T"
+	fi
+	count=$(($count+1))
+done
+
+echo ${dict[@]}
+echo $num
+
+echo "%age of Head: "
+echo $(($HEAD*100/$num)).$(( ( ($HEAD*100*100)/$num ) % 100 ))
+echo "%age of Tail: "
+echo $(($TAIL*100/$num)).$(( ( ($TAIL*100*100)/$num ) % 100 ))
